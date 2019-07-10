37e9b0b244c3035bab57528dcf6472691d5cf85d
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-10 03:27:26 +0000

    Done.

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,10 @@
+module Findable
+  def find_by_name(name)
+    @@self.class.detect{|a| a.name == name}
+    # @@artists.detect{|a| a.name == name}
+  end
+end
+
+# self = @@artists? or self.class.all???
+#@@self.detect
+#use global variables? $
