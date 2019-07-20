242b89f170f296db3b736101fba2b5533221f438
Maasa Kono <konomaasa@gmail.com>
2019-07-20 02:27:02 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,27 @@
 require 'pry'
 
 class Artist
+
+  extend Memorable, Findable
+  # extend Findable
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
-  def initialize
+  def initialize(name)
+    @name = name
     @@artists << self
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
