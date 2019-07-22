1e4cf4cc8e34a730b06bfb37890f2b8e20f64b24
Chris Junker <elevatemedfit@gmail.com>
2019-07-22 14:31:46 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,14 @@
 require 'pry'
 
 class Song
+
+  extend Memorable
+  extend Findable
+  include Paramable
+
+
   attr_accessor :name
-  attr_reader :artist
+  attr_reader :artist, :songs ##Added songs to Song making Class module work on find_by
 
   @@songs = []
 
@@ -10,27 +16,31 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def reset_all
+  #    self.all.clear
+  #  end
+  #
+  #  def count
+  #    self.all.count
+  #  end
+
+  #  def find_by_name(name)
+  #    self.detect{|a| a.name == name}
+  # end
+
+  #  def self.find_by_name(name)
+  #    @@songs.detect{|a| a.name == name}
+  #  end
 
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
